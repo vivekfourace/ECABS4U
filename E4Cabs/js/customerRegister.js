c9de6d407ec21dc59f7b6b612668b6cf8f3a9b75
Naveen Singh <itsnaveen@yahoo.in>
2013-08-19 09:06:05 -0500

    commited on 19th aug by naveen

--- E4Cabs/js/customerRegister.js
+++ E4Cabs/js/customerRegister.js
@@ -57,6 +57,7 @@ function Registercustomer()
                     $('#lblEmail').text("*please enter the Email Address");
                     return false;
                 }
+    
               if(txt5.length > 0)
                     {
                         $('#lblUserID').text(" ");
---
+++ E4Cabs/js/driverStatusUpdate.js
@@ -0,0 +1,22 @@
+
+//Fetch status and update the label: ID = lblCurrentStatus of driver
+
+function engage()
+{
+    alert('engage');
+}
+
+function available()
+{
+    alert('available');    
+}
+
+function onTheWay()
+{
+    alert('ontheway');    
+}
+
+function rejected()
+{
+          alert('rejected');  
+}
