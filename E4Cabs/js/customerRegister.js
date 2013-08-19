1c4c00484b451923f57185a157f1bcd5bf5d78c9
Naveen Singh <itsnaveen@yahoo.in>
2013-08-19 03:13:12 -0500

    Changes made by Naveen Singh (itsnaveen@yahoo.in).

--- E4Cabs/js/customerRegister.js
+++ E4Cabs/js/customerRegister.js
@@ -9,44 +9,58 @@ function Registercustomer()
     var txt6 =$('#txt6').val();
     var txt7 =$('#txt7').val();
     var txt8 =$('#txt8').val();
-    var atpos=txt4.indexOf("@");
-    var dotpos=txt4.lastIndexOf(".");
+    //var atpos=txt4.indexOf("@");
+    //var dotpos=txt4.lastIndexOf(".");
+    var regExpEmail = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$";
     var phoneno =/^\d{12}$/;
     
                if(!txt1)
                  {
-                     $('#lblFirstName').text("*please enter the First Name");
+                     $('#lblFirstName').text("*please enter the first name");
                     return false;
                  }
                if(!txt2)
                  {
-                     $('#lblLastName').text("*please enter the Last Name");
+                     $('#lblLastName').text("*please enter the last name");
                     return false;
                  }
-             if(!txt3)
-                 {
-                     $('#lblPhoneNo').text("*please enter the Phone Number");
-                    return false;
-                 }
-               if(phoneno.test(txt3))
+                //validate Phone number.
+               if(txt3.length > 0)
+                    {
+                        if(phoneno.test(txt3))
+                        {
+                            $('#lblPhoneNo').text(" ");
+                        }
+                        else
+                        {
+                             $('#lblPhoneNo').text("*Please enter valid phone number");
+                            return false;
+                        }
+                    }
+                else if(txt3.length == 0)
                 {
-                 
-                }
-                else
-                {
-                    $('#lblphoneVerification').text("***Not a valid Mobile No.***");
-                    return false; 
-                }
-                if(!txt4)
-                 {
-                     $('#lblEmail').text("*please enter the Email Address");
+                     $('#lblPhoneNo').text("*please enter the phone number");
                     return false;
-                 }
-                 if (atpos<1 || dotpos<atpos+2 || dotpos+2>=txt4.length)
-                      {
-                         $('#lblEmailVerification').text("Not a valid e-mail address");
-                         return false;
-                       }
+                }
+    
+                //validate Email address
+                if(txt4.length > 0)
+                    {
+                        if(txt4.match(regExpEmail))
+                        {
+                            $('#lblEmail').text(" ");
+                        }
+                        else{
+                            $('#lblEmail').text("*please enter a valid Email address");
+                            return false;
+                        }
+                    }
+                else if(txt4.length == 0)
+                {
+                    $('#lblEmail').text("*please enter the Email Address");
+                    return false;
+                }
+              
             if(!txt5)
                  {
                      $('#lblUserID').text("*please enter the User ID");
