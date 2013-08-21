e1c96e1c58d8592738c2a7a7b5e96dc52f23d3d5
Raghavendra m.bhat <raghuhan99@gmail.com>
2013-08-21 06:57:08 -0500

    Changes made by Raghavendra m.bhat (raghuhan99@gmail.com).

--- E4Cabs/js/login.js
+++ E4Cabs/js/login.js
@@ -1,24 +1,25 @@
  function login()
             {
+               
                 var name=document.getElementById('txtUserName').value;
-                var password=document.getElementById('txtPassword').value;
+                var Password=document.getElementById('txtPassword').value;
                 if(name.length > 0)
                 {
                     $('#lblName').text("");
-                    return true;
+                   
                 }
                 else if(name.length==0)
                 {
                 $('#lblName').text("Please enter username.");
                  return false;
                 }
-              if(password.length>0)
+              if(Password.length>0)
                 {
                     $('#lblPassword').text("");
-                    return true;
+                    
                     
                 }
-                 else if(password.length==0) 
+                 else if(Password.length==0) 
                 {
                     $('#lblPassword').text("Please enter password.");
                      return false;
@@ -28,16 +29,13 @@
                 $.ajax(url,{                      
                      type:"POST",
                      datatype:"json",
-                     data:"{'username':'"+name+"','userpassword':'"+Password+"'}",
+                     data: "{'username':'" + name + "','userpassword':'" + Password + "'}",
                      contentType: "application/json; charset=utf-8",                     
                      success: function(data)
                      {
-                         alert(data);
-                        window.location="customerRegister.html";
-                     },
-                   error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    alert(errorThrown);
-                }
+                         alert("data");
+                        window.location="driverStatusUpdate.html";
+                     }
                
                  });
                 }
