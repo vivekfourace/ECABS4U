b6e7a06895d4dd6138dcda4236c82cb6edabccfb
Naveen Kumar <naveen.jp02888@gmail.com>
2013-09-02 08:53:50 -0500

    Changes made by Naveen Kumar (naveen.jp02888@gmail.com).

--- E4Cabs/js/forgotPassword.js
+++ E4Cabs/js/forgotPassword.js
@@ -25,9 +25,21 @@ function forgotPassword()
                 
                 $('#imgLoader').bind('ajaxStart', function(){
                     $(this).show();
+                    showDisableLayer();
                  }).bind('ajaxStop', function(){
                     $(this).hide();
+                     hideDisableLayer();
                 });
+                
+                  showDisableLayer = function() {
+                  $('<div id="loading" style="position:fixed; z-index: 2147483647; top:0; left:0; background-color: #7B7F86; opacity:0.4;filter:alpha(opacity=0);"></div>').appendTo(document.body);
+                  $("#loading").height($(document).height());
+                  $("#loading").width($(document).width());
+                };
+
+                    hideDisableLayer = function() {
+                    $("#loading").remove();
+                };
               
                 $.ajax({
                     url:"http://115.115.159.126/ECabs/ECabs4U.asmx/forgotMyPwd",
@@ -36,9 +48,6 @@ function forgotPassword()
                     data:"{'emailid':'"+pass+"'}",
                    contentType: "application/json; charset=utf-8", 
                     success: CheckMsg,
-                    complete: function(){
-                            $('#imgLoader').hide();
-                              },
                     error: function (XMLHttpRequest, textStatus, errorThrown) {
                             alert(errorThrown);
                        }
@@ -51,6 +60,7 @@ function forgotPassword()
                 $('#lblMsg').text("Incorrect Email id");
                 $('#lblMsg').css("color","#D70007");
                 $('#lblMsg').css("font-size","13");
+                $('#txtEmail').val("");
                 
             }
             else if (data.d == "false2") {
