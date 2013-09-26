d5d2c37f722a2efaebfad10a6814a3b45338637e
Naveen Kumar <naveen.jp02888@gmail.com>
2013-09-26 07:04:17 -0500

    Changes made by Naveen Kumar (naveen.jp02888@gmail.com).

--- E4Cabs/js/customerSearch.js
+++ E4Cabs/js/customerSearch.js
@@ -1,8 +1,7 @@
-   var QString = window.location.search.substring(1);
+var QString = window.location.search.substring(1);
 var userId =  QString.split("=")[1].split("&")[0];
 var roleId = QString.split("=")[2].split("&")[0];
 var relatedId = QString.split("=")[3].split("&")[0];
-
 
 function loc()
 {
@@ -20,18 +19,13 @@ function backtoCustomerhome()
  // window.location="customerHome.html";
 }
 
-
-
 //current Location
 function currentlocation()
 {
   //window.location =  'Location.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;  
 }
 
-
-
 //Search Available
-
 
 function availabledriver()
 {
@@ -47,59 +41,58 @@ function availabledriver()
             var scase = document.getElementById("ddlsmallcase"); 
             var smallcase=scase.options[scase.selectedIndex].value;
              
-             var returnDate = document.getElementById("datepickers").value;
-             var returnTime = document.getElementById("timepickers").value;
+            var returnDate = document.getElementById("datepickers").value;
+            var returnTime = document.getElementById("timepickers").value;
     
-    //add Location
+            //add Location    
             var secondLoc=document.getElementById("txt2location").value;
-            var thirdLoc=document.getElementById("txt3location").value;    
-    //special
-    var weelchairPassangers=document.getElementById("ddlWheelchair");
-    var WchairPassengers=weelchairPassangers.options[weelchairPassangers.selectedIndex].value;
+            var thirdLoc=document.getElementById("txt3location").value;
     
-    var childS=document.getElementById("ddlChidseats");
-    var childSeats=childS.options[childS.selectedIndex].value; 
+            //special
+            var weelchairPassangers=document.getElementById("ddlWheelchair");
+            var WchairPassengers=weelchairPassangers.options[weelchairPassangers.selectedIndex].value;
+            
+            var childS=document.getElementById("ddlChidseats");
+            var childSeats=childS.options[childS.selectedIndex].value; 
+            
+            var childB = document.getElementById("ddlChidbooster");
+            var childBooster=childB.options[childB.selectedIndex].value;  
+            
+            var otherSpeRequirement = document.getElementById('txtothereSpecialRequirement').value;
+            
+            var IsReturnTrue = $('#chkReturnYes').attr('checked')?true:false;   
     
-    var childB = document.getElementById("ddlChidbooster");
-    var childBooster=childB.options[childB.selectedIndex].value;  
+            //Return
     
-    var otherSpeRequirement = document.getElementById('txtothereSpecialRequirement').value;
+            var returnfromloc=document.getElementById('txtReturFrom').value;
+            var returntoloc=document.getElementById('txtReturTo').value;
     
-    var IsReturnTrue = $('#chkReturnYes').attr('checked')?true:false;   
-    
-    //Return
-    
-   var returnfromloc=document.getElementById('txtReturFrom').value;
-    var returntoloc=document.getElementById('txtReturTo').value;
-    
-    if(!fromloc)
-    {
-        $('#lblMessage').text("Enter From location!");
-        return false;
-    }
-    if(!toloc)
-    {
-        $('#lblMessage').text("Enter To location!"); 
-        return false;
-    }
+            if(!fromloc)
+            {
+                $('#lblMessage').text("Enter From location!");
+                return false;
+            }
+            if(!toloc)
+            {
+                $('#lblMessage').text("Enter To location!"); 
+                return false;
+            }
 
-    $.ajax({
-     url: "http://115.115.159.126/ECabs/ECabs4U.asmx/CustomerRequest",
-       type:"POST",
-       dataType: "Json",
-       data:"{'userID':'" +relatedId+"','frompost':'"+ fromloc +"','topost':'"+ toloc +"','pickDate':'"+ pickdate +"','pickTime':'"+ picktime +"','passenger':'"+ totalpassenger +"','lcase':'"+ largecase +"','scase':'"+ smallcase +"','distance':'"+ distance +"','secondL':'"+secondLoc+"','thirdLoc':'"+thirdLoc+"','WchairPassengers':'"+WchairPassengers+"','childSeats':'"+childSeats+"','childBooster':'"+childBooster+"','otherSpeRequirement':'"+otherSpeRequirement+"','IsReturnTrue':'"+IsReturnTrue+"','returnfromloc':'"+returnfromloc+"','returntoloc':'"+returntoloc+"','returnDate':'"+returnDate+"','returnTime':'"+returnTime+"'}",
-       contentType: "application/json; charset=utf-8",  
-       success: function (data) {
-           var reqID = data.d;
-           //window.location='RolePop.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
-           window.location='customerSearchList.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId+'&reqid='+reqID;
-    
-       },
-       error: function (XMLHttpRequest, textStatus, errorThrown) {
-           alert(errorThrown);
-       }
-    });
-}
+            $.ajax({
+             url: "http://115.115.159.126/ECabs/ECabs4U.asmx/CustomerRequest",
+               type:"POST",
+               dataType: "Json",
+               data:"{'userID':'" +relatedId+"','frompost':'"+ fromloc +"','topost':'"+ toloc +"','pickDate':'"+ pickdate +"','pickTime':'"+ picktime +"','passenger':'"+ totalpassenger +"','lcase':'"+ largecase +"','scase':'"+ smallcase +"','distance':'"+ distance +"','secondL':'"+secondLoc+"','thirdLoc':'"+thirdLoc+"','WchairPassengers':'"+WchairPassengers+"','childSeats':'"+childSeats+"','childBooster':'"+childBooster+"','otherSpeRequirement':'"+otherSpeRequirement+"','IsReturnTrue':'"+IsReturnTrue+"','returnfromloc':'"+returnfromloc+"','returntoloc':'"+returntoloc+"','returnDate':'"+returnDate+"','returnTime':'"+returnTime+"'}",
+               contentType: "application/json; charset=utf-8",  
+               success: function (data) {
+                   var reqID = data.d;
+                   window.location='customerSearchList.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId+'&reqid='+reqID;
+               },
+               error: function (XMLHttpRequest, textStatus, errorThrown) {
+                   alert(errorThrown);
+               }
+            });
+        }
 
 
 //My Profile Button
@@ -107,8 +100,6 @@ function availabledriver()
             {
                 window.location =  'customerHome.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
             }
-
-
 
 //Logout Button
 function logout()
@@ -122,10 +113,8 @@ function logout()
 //Cab Now Button
 function cabNow()
 { 
-  
    window.location ='customerSearch.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
 }
-
 
 //Customer Feedback 
 function feedBack()
@@ -133,20 +122,12 @@ function feedBack()
     window.location='customerFeedback.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
 }
 
-
-
-
 //Booked History
 function bookedHistory()
 {
  //alert(relatedId);
- 
   window.location='bookedhistory.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
-   
 }
-
-
-
 
 //from Menu
 function preCab()
--- E4Cabs/js/customerSearchList.js
+++ E4Cabs/js/customerSearchList.js
@@ -4,10 +4,100 @@ var roleId = QString.split("=")[2].split("&")[0];
 var relatedId = QString.split("=")[3].split("&")[0];
 var requestID = QString.split("=")[4].split("&")[0];
 
-
 function backtosearch()
 {
     window.location = 'customerSearch.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
+}
+
+//alert(userId + roleId + relatedId + requestID);
+        
+         ////Ajax loader--start
+         //     $('#imgLoader').bind('ajaxStart', function(){
+         //         $(this).show();
+         //         showDisableLayer();
+         //      }).bind('ajaxStop', function(){
+         //         $(this).hide();
+         //          hideDisableLayer();
+         //     });
+         //     
+         //       showDisableLayer = function() {
+         //       $('<div id="loading" style="position:fixed; z-index: 2147483647; top:0; left:0; background-color: #E6E6E6; opacity:0.4;filter:alpha(opacity=0);"></div>').appendTo(document.body);
+         //       $("#loading").height($(document).height());
+         //       $("#loading").width($(document).width());
+         //     };
+
+         //         hideDisableLayer = function() {
+         //         $("#loading").remove();
+         //     };
+         //   //Ajax loader--ends
+
+
+  $.ajax({
+    url: "http://115.115.159.126/ECabs/ECabs4U.asmx/GetResponseData",
+     type:"POST",
+     dataType: "Json",
+     data:"{'requestID':'" +requestID+"'}",
+     contentType: "application/json; charset=utf-8",  
+     success: getData,
+     error: function (XMLHttpRequest, textStatus, errorThrown) {
+          alert(errorThrown);
+      }
+   });
+
+
+function getData(data)
+{
+    var count = data.d.length;
+    alert(count);
+    $('#msg').append('<table style="width:100%" cellspacing="1" border="1px solid cyan">')
+    .append('<thead style="background-color:LightGray;" valign="left">')
+    .append('<tr>')
+    .append('<th style="width:25%;height:25px">Name</th>')
+    .append('<th style="width:15%">Fare</th>')
+    .append('<th style="width:20%">Date</th>')
+    .append('<th style="width:20%">Time</th>')
+    .append('<th style="width:20%"></th>')
+    .append('</tr>')
+    .append('</thead>')
+                   .append('<tbody>')     
+                        for(var i=0; i<count; i++)
+                        {
+                            var driverID = data.d[i]["DriverID"];
+                            var customerReqId = data.d[i]["CustomerRequestID"];
+                            $('#msg').append('<tr>')
+                            .append("<td width='25%' align='center'>" + data.d[i]["DriverName"] + "</td>")
+                            .append("<td width='25%' align='left'>" + data.d[i]["DriverName"] +"</td>")
+                            .append("<td width='25%' align='left'>" + data.d[i]["StartDate"] +"</td>")
+                            .append("<td width='25%' align='left'>" + data.d[i]["StartTime"] +"</td>")
+                            .append("<td width='25%' align='center'>" + '<input type="button" value="Hire" id= "'+ driverID +'" onclick = "Hireme(\''+driverID+'\',\''+customerReqId+'\'); return false;" title= "'+driverID+'" />' + "</td>")
+                            .append('</tr>')
+                        }
+                   $('#msg').append('</tbody>')
+   $('#msg').append('</table>');
+ }
+
+function Hireme(driID, reqID)
+{
+    var driverId = driID;
+    var requestId = reqID;
+    alert(driID);
+    alert(reqID);
+    $.ajax({
+       url: "http://115.115.159.126/ECabs/ECabs4U.asmx/HireDriverResponse",
+       type:"POST",
+       dataType: "Json",
+       data:"{'driverId':'" +driverId+"','requestId':'"+ requestId +"'}",
+       contentType: "application/json; charset=utf-8",  
+       success:getResponseFromDriver ,
+       error: function (XMLHttpRequest, textStatus, errorThrown)
+                {
+                   alert(errorThrown);
+               }
+   });
+}
+
+function getResponseFromDriver(data)
+{
 }
 
 
