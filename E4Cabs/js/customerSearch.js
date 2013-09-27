f1bce2f73075d2fff77e62c9a43a4ba16312a57e
Raghavendra Bhat <raghuhan100@hotmail.com>
2013-09-27 06:49:19 -0500

    Changes made by Raghavendra Bhat (raghuhan100@hotmail.com).

--- E4Cabs/js/customerSearch.js
+++ E4Cabs/js/customerSearch.js
@@ -30,6 +30,8 @@ function currentlocation()
 
 function availabledriver()
 {
+    
+    
             var fromloc=document.getElementById('txtFrom').value;
             var toloc=document.getElementById('txtTo').value;
             var distance=document.getElementById('txtDistance').value;
@@ -80,7 +82,7 @@ function availabledriver()
             }
 
             $.ajax({
-            // url: "http://115.115.159.126/ECabs/ECabs4U.asmx/CustomerRequest",
+            url: "http://115.115.159.126/ECabs/ECabs4U.asmx/CustomerRequest",
                type:"POST",
                dataType: "Json",
                data:"{'userID':'" +relatedId+"','frompost':'"+ fromloc +"','topost':'"+ toloc +"','pickDate':'"+ pickdate +"','pickTime':'"+ picktime +"','passenger':'"+ totalpassenger +"','lcase':'"+ largecase +"','scase':'"+ smallcase +"','distance':'"+ distance +"','secondL':'"+secondLoc+"','thirdLoc':'"+thirdLoc+"','WchairPassengers':'"+WchairPassengers+"','childSeats':'"+childSeats+"','childBooster':'"+childBooster+"','otherSpeRequirement':'"+otherSpeRequirement+"','IsReturnTrue':'"+IsReturnTrue+"','returnfromloc':'"+returnfromloc+"','returntoloc':'"+returntoloc+"','returnDate':'"+returnDate+"','returnTime':'"+returnTime+"'}",
