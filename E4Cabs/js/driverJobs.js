a7bc382b7c06419024f1b4abaa6741812e7aa6cc
vikash Prasun <vikash.prasun1990@gmail.com>
2013-10-05 01:35:12 -0500

    Changes made by vikash Prasun (vikash.prasun1990@gmail.com).

--- E4Cabs/js/driverJobs.js
+++ E4Cabs/js/driverJobs.js
@@ -3,23 +3,56 @@ var userId =  QString.split("=")[1].split("&")[0];
 var roleId = QString.split("=")[2].split("&")[0];
 var relatedId = QString.split("=")[3].split("&")[0];
 
+function showMap()
+{
+   var from = $('#lblFromLoc').text();
+   var to = $('#lblToLoc').text();
+   var dis = "loc"; //it will specify later;
+   window.location =  'Location.html?id='+from+'&rid='+to+'&rrid='+dis;
+}
+
 function BackProfile()
 {
     window.location='driverHome.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
 }
 
 
+
+
+
 //diver Profile from menu
 function myProfile()
+
 {
+    
     window.location='driverProfile.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
 }
 
 //Driver Jobs from menu
 function MyBookings(){
-    window.location='driverJobs.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
+    window.location='DriverJob.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
 }
 
+function bid()
+{
+    var rid =relatedId ;
+    var status=true; 
+    var price=50;
+     $.ajax({
+           url: "http://115.115.159.126/ECabs/ECabs4U.asmx/setDriverResponse",
+           type:"POST",
+           dataType: "Json",
+           data:"{'userID':'" + relatedId +"','reqid':'"+ rid +"','status':'"+ status  +"','price':'"+ price +"'}",
+           contentType: "application/json; charset=utf-8",  
+           success: function (data) 
+                       {
+                       },
+           error: function (XMLHttpRequest, textStatus, errorThrown)
+                    {
+           //alert(errorThrown);
+            }
+       });
+    }
 
 //driver Logout
 function logout(){
@@ -32,7 +65,7 @@ function logout(){
 
 
 //Driver Status
-function HomePage(){
+function myhome(){
     window.location='driverHome.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;
 }
 
@@ -49,5 +82,7 @@ function feedBack()
 //History
 function bookedHistory()
 {
+    
   window.location='driverHistory.html?id='+userId+'&rid='+roleId+'&rrid='+relatedId;  
 }
+
