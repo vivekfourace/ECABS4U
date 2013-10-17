0d9756b35e59015af369ee148825bd04d8971772
James Cam <jamescam59@gmail.com>
2013-10-17 08:36:32 -0500

    Changes made by James Cam (jamescam59@gmail.com).

--- E4Cabs/js/driverHome.js
+++ E4Cabs/js/driverHome.js
@@ -3,9 +3,57 @@ var userId =  QString.split("=")[1].split("&")[0];
 var roleId = QString.split("=")[2].split("&")[0];
 var relatedId = QString.split("=")[3].split("&")[0];
 
+setInterval(getCurrentPostCode, 300000); 
+
+        function getCurrentPostCode() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    var geocoder = new google.maps.Geocoder();
+                    var latLng = pos;
+                    geocoder.geocode({ 'latLng': latLng }, function (results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            if (results[0]) {
+                                var postalCode = extractFromAdress(results[0].address_components, "postal_code");
+                                alert("Your Postcode is: " + postalCode);
+
+                                function extractFromAdress(components, type) {
+                                    for (var i = 0; i < components.length; i++)
+                                        for (var j = 0; j < components[i].types.length; j++)
+                                            if (components[i].types[j] == type)
+                                                return components[i].long_name;
+                                    return "";
+                                }
+                                updatePostCode(postalCode);
+                            }
+                        }
+
+                    });
+                });
+            }
+        }
+
+        function updatePostCode(postalCode) {
+           // alert(postalCode)
+           // alert(relatedId)
+           // alert('hi');
+            $.ajax({
+                url: 'http://115.115.159.126/ECabs/ECabs4U.asmx/updateLocationMapping',
+                type: "post",
+                dataType: "json",
+                data: "{'postalCode':'" + postalCode + "','Id':'" + relatedId + "'}",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    //alert(data.d);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    //alert(errorThrown);
+                }
+            });
+        }
 function Check(){
+    //getCurrentPostCode();
    // $("body").css({visibility:'none '})
-   //  alert("tbz");
      var url = "http://115.115.159.126/ECabs/ECabs4U.asmx/userStatus";
                 $.ajax(url,{                      
                      type:"POST",
@@ -20,7 +68,6 @@ function Check(){
                          $('#driverStatusupdate').text("Available");
                              
                               $.ajax({
-                                          
                                            url:'http://115.115.159.126/ECabs/ECabs4U.asmx/CheckNewJob', 
                                            type:"POST",
                                            datatype:"json",
